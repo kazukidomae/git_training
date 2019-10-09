1fdfd3918c4d178fa087e05d454ae9fba680cbcb
morrisharuka <yako0073@gmail.com>
2019-10-09 18:36:25 +0900

    aaaaaaa

--- php/info.php
+++ php/info.php
@@ -1,7 +1,6 @@
 <?php
-    $url = "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=e92ce33edab8424b
-    &id".$id."&name=".$name."name_kana=".$kana."name_any=".$any."tel=".$tel.
-    "&format=json";
+    $id="";
+    $url = "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=&large_area=Z011&id=".$id."&format=json";
     //検索条件を変数化することで受け取った変数を検索条件に加えられる
     $json = file_get_contents($url);
     $json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
@@ -10,9 +9,10 @@
     if ($arr === NULL) {
         echo("応答なし");
     }else{
-        echo ("<pre>");
-        var_dump($arr["results"]["shop"][0]["id"]);//デバッグ用なので実際は使わない
-        echo ("</pre>"); 
-        echo ($arr["results"]["shop"][0]["name_kana"]);//APIから受け取った情報を絞り込んで表示
+        $i=0;
+        while($arr["results"]["shop"][$i]!=NULL){
+            echo ($arr["results"]["shop"][$i]["name"]."<br/>"); //APIから受け取った情報を絞り込んで表示
+            $i++;
+        }
     }
 ?>
