8a9d3527aaeb5b43918dea947512d1b1d813f0ad
morrisharuka <yako0073@gmail.com>
2019-10-09 17:43:28 +0900

    aaaaaaa

--- php/info.php
+++ php/info.php
@@ -1,6 +1,8 @@
 <?php
-
-    $url = "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=e92ce33edab8424b&format=json";
+    $url = "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=e92ce33edab8424b
+    &id".$id."&name=".$name."name_kana=".$kana."name_any=".$any."tel=".$tel.
+    "&format=json";
+    //検索条件を変数化することで受け取った変数を検索条件に加えられる
     $json = file_get_contents($url);
     $json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
     $arr = json_decode($json,true);
@@ -8,14 +10,9 @@
     if ($arr === NULL) {
         echo("応答なし");
     }else{
-        echo("応答あり");
-        $bc_id = array();
-        $bc_title = array();
-        $bc_write_day_year = array();
-        $bc_write_day_month = array();
-        $bc_write_day_day = array();
-        $bc_author = array();
-        $bc_tag = array();
-        $bc_report = array();
+        echo ("<pre>");
+        var_dump($arr["results"]["shop"][0]["id"]);//デバッグ用なので実際は使わない
+        echo ("</pre>"); 
+        echo ($arr["results"]["shop"][0]["name_kana"]);//APIから受け取った情報を絞り込んで表示
     }
 ?>
